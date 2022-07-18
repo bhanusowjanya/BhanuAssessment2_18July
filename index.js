03b734c1f4941500dd9074cfe1342e84098d72ec
Bhanu Sowjanya <bhanusowjanya.paila@gmail.com>
2022-07-19 00:29:08 +1000

    Assesment 2 submission

---
+++ index.js
@@ -0,0 +1,14 @@
+function validFormFieldInput()
+{
+    const trip = document.getElementById("trip");
+    console.log(trip);
+}
+
+function validFormFieldInput()
+{
+    const nameOne = document.getElementById("name1");
+    if (nameOne.length <= 0) {
+        alert ("Name should be atleast five charecters");
+    }
+    
+}
